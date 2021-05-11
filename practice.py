a0a38322b5415c6f91c148fd209cc5b22bd53b97
YueliZhu <yueli.zhu@canada.ca>
2021-05-11 08:08:24 -0700

    Python practice-Init commit

---
+++ practice.py
@@ -0,0 +1,72 @@
+#String Formatting
+
+# principle = 598.36
+# ratio = 9.678
+# interest = principle * ratio
+
+# print(f"The interest is {interest:.2f}")
+
+# sentence = "It's raining cats and cats"
+# # # print (sentence.endswith("cats"))
+
+# # print(sentence.index("cats"))
+
+# print(type(sentence))
+
+# sentence = ("It","is","a","sentence")
+
+# print(type(sentence))
+
+
+# sentence = ["It","is","a","sentence"]
+
+# print(type(sentence))
+
+# sentence.pop(3)
+
+# print(sentence)
+
+# sentence.append("sentence")
+
+# print(sentence)
+
+# sentence[3]="change"
+
+# print(sentence)
+
+# sentence.insert(3,"new")
+
+# print(sentence)
+
+# def skip_elements(elements):	
+# 	# for element in elements:
+# 	# 	return elements[::2]
+# 	ele = list(enumerate(elements))
+   
+# 	for index, element in ele:
+
+# 	    print(index,element)
+
+
+# skip_elements(["a", "b", "c", "d", "e", "f", "g"])
+
+# def skip_elements(elements):
+#    
+#     for element in elements:
+# 	 return elements[::2]
+
+# print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+# print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+# def skip_elements(elements):
+#     mylist = []
+#     for index, element in enumerate(elements):
+#         if index%2 == 0:
+#             mylist.append(element)
+#     return mylist
+
+# print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+# print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+ 
+
+    
